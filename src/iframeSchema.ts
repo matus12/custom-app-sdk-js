d0b7233a3075854dbbdc5d49a204853870559898
Matus Backor <matusb@kontent.ai>
2024-12-03 14:24:32 +0100

    Update schema to be up to date

--- src/iframeSchema.ts
+++ src/iframeSchema.ts
@@ -53,75 +53,11 @@ const ClientInitV1Response = z
   .or(ErrorMessage)
   .readonly();
 
-const ClientInitV2Request = z
-  .object({
-    type: z.literal('init-request'),
-    requestId: z.string().uuid(),
-    payload: z.object({ p: z.string() }).readonly(),
-    version: z.literal('2.0.0'),
-  })
-  .readonly();
-
-const ClientInitV2Response = z
-  .object({
-    type: z.literal('init-response'),
-    payload: z
-      .object({
-        context: z.object({}).readonly(),
-        config: z.object({}).readonly().or(z.null()),
-      })
-      .readonly(),
-    requestId: z.string().uuid(),
-    version: z.literal('2.0.0'),
-  })
-  .or(ErrorMessage)
-  .readonly();
-
-const GetV1Request = z
-  .object({
-    type: z.literal('get-request'),
-    requestId: z.string().uuid(),
-    version: z.literal('1.0.0'),
-    payload: z.null(),
-  })
-  .readonly();
-
-const GetV1Response = z
-  .object({
-    type: z.literal('get-response'),
-    requestId: z.string().uuid(),
-    version: z.literal('1.0.0'),
-    payload: z.null(),
-  })
-  .or(ErrorMessage)
-  .readonly();
-
-const HostItemDeletedRequest = z.object({
-  type: z.literal('item-deleted-request'),
-  requestId: z.string().uuid(),
-  version: z.literal('1.0.0'),
-  payload: z.object({ itemId: z.string().uuid() }).readonly(),
-});
-
 export type Schema = {
   client: {
     'init@1.0.0': {
       request: z.infer<typeof ClientInitV1Request>;
       response: z.infer<typeof ClientInitV1Response>;
-    };
-    'init@2.0.0': {
-      request: z.infer<typeof ClientInitV2Request>;
-      response: z.infer<typeof ClientInitV2Response>;
-    };
-    'get@1.0.0': {
-      request: z.infer<typeof GetV1Request>;
-      response: z.infer<typeof GetV1Response>;
-    };
-  };
-  host: {
-    'item-deleted@1.0.0': {
-      request: z.infer<typeof HostItemDeletedRequest>;
-      response: null;
     };
   };
 };
